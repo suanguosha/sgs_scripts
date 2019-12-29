c044049d2e6f92a69bab45ed8c6c56b806195b57
Dayu Liu <36178461+LDY681@users.noreply.github.com>
2019-12-29 22:02:20 +0800

    Update 新逐鹿天下120关(自走棋).js

--- 新逐鹿天下120关(自走棋).js
+++ 新逐鹿天下120关(自走棋).js
@@ -1,8 +1,9 @@
 /*使用方法
-先进入游戏，以后按ctrl+shift+i呼出控制台
-然后按ctrl+shift+c呼出检视工具，和游戏界面互动一下（比如点聊天框）
-在控制台输入TableGameManager，如果没有报错，那么就可以了。如果有报错，继续呼啦。
-将以下代码一并复制黏贴，并回车运行*/
+step 1: 先进入游戏，进入逐鹿天下（有的界面开控台会重定向回登录，所以记得先进逐鹿），然后按ctrl+shift+i呼出控制台
+step 2: 在控制台输入TableGameManager，如果没有报错，看step 4。如果有报错，看step 3。
+step 3: 按ctrl+shift+c呼出检视工具（不同浏览器的监视工具快捷键不同，自己百度一下）。和游戏界面互动一下（比如点聊天框），重复step 2
+step 4：将以下代码一并复制黏贴，并回车运行
+*/
 
 var startInterval = setInterval(function () {
     if (!SceneManager.GetInstance().CurrentScene.manager) { //如果不在游戏中
