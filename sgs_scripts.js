d044d1963712d6f00e5b8464966bc2c4225d23c3
Dayu Liu <36178461+LDY681@users.noreply.github.com>
2020-01-05 16:24:05 +0800

    update

--- sgs_scripts.js
+++ sgs_scripts.js
@@ -29,12 +29,12 @@ function zhuLu(){
         var startInterval = setInterval(function () {
             if (!SceneManager.GetInstance().CurrentScene.manager) { //如果不在游戏中
                 // 不在逐鹿天下模式下进入
-                if (SceneManager.GetInstance().CurrentScene.sceneName != 'NewCompeteWorldScene') {
+                if (SceneManager.GetInstance().CurrentScene.sceneName !== 'NewCompeteWorldScene') {
                     RoomControler.GetInstance().EnterMode(ModeIDType.MITZhuLuTianXiaNew);
                     return;
                 }
                 // 120关
-                var towerLevelID = ParseInt(towerLevel) ? ParseInt(towerLevel): NewCompeteWorldManager.GetInstance().competeWorldInfo.curTowerLevelID;
+                var towerLevelID = parseInt(towerLevel,10) ? parseInt(towerLevel,10): NewCompeteWorldManager.GetInstance().competeWorldInfo.curTowerLevelID;
                 var generalList = NewCompeteWorldManager.GetInstance().GetBattleGeneralListForTemp(this.MaxGeneralCount);
                 NewCompeteWorldManager.GetInstance().ReqCompeteWorldBattle(towerLevelID, generalList);
             }else{  //如果在游戏中
@@ -57,8 +57,6 @@ function riChang(){
 
 //每日签到
     DailySignManager.GetInstance().ReqGetSignInReward(1, DailySignManager.GetInstance().initSignDate);
-
-//TODO 如果有周奖励 自动领取
 
 //公会敲鼓3次
     for (var i = 0; i < 3; i++) {
@@ -115,7 +113,28 @@ function chat(){
     if (chatMaxCount === null){
         return main();
     }
-    var channelType = getChannel(parseInt(chatChannel));
+    var channelType;
+    var channelId = parseInt(chatChannel,10);
+        //综合：WORLD, 公会：GUILD 房间：ROOM,私聊：PERSONAL, 队伍：TEAM, 势力: COUNTRY , 诏令: ZHAO
+        switch (channelId){
+            case 1:
+                channelType = ChatChannelType.WORLD;
+            case 2:
+                channelType = ChatChannelType.GUILD;
+            case 5:
+                channelType = ChatChannelType.ROOM;
+            case 6:
+                channelType = ChatChannelType.TEAM;
+            case 4:
+                channelType = ChatChannelType.COUNTRY;
+            case 3:
+                channelType = ChatChannelType.ZHAO;
+            case 7:
+                channelType = ChatChannelType.PERSONAL;
+            default:
+                main();
+        }
+
     var count = 0;
     var interval = setInterval(function(){
         if (count === parseInt(chatMaxCount, 10)){
@@ -131,24 +150,3 @@ function hongBao(){
     main();
 }
 
-function getChannel(chatChannel){
-    //综合：WORLD, 公会：GUILD 房间：ROOM,私聊：PERSONAL, 队伍：TEAM, 势力: COUNTRY , 诏令: ZHAO
-    switch (chatChannel){
-        case 1:
-            return ChatChannelType.WORLD;
-        case 2:
-            return ChatChannelType.GUILD;
-        case 5:
-            return ChatChannelType.ROOM;
-        case 6:
-            return ChatChannelType.TEAM;
-        case 4:
-            return ChatChannelType.COUNTRY;
-        case 3:
-            return ChatChannelType.ZHAO;
-        case 7:
-            return ChatChannelType.PERSONAL;
-        default:
-            main();
-    }
-}
