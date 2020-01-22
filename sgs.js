f062c1753186664444aa20906bc30b2ac8cfb854
LDY681 <ldydyx@gmail.com>
2020-01-21 20:30:46 -0500

    新增自动登录

--- sgs.js
+++ sgs.js
@@ -1,14 +1,29 @@
 $(document).ready(function(){
-    var login = confirm("欢迎使用代码杀：登录点确定，注册点取消");
-    if (!login){
+    var currUser = localStorage.getItem("AVusername");
+    var currPass =  localStorage.getItem("AVpassword");
+    var message;
+    var username;
+    var password;
+    if (!currUser || !currPass){    //如果没有user和password
+        message = "欢迎使用代码杀：登录请按1，注册请按2"
+    }else{
+        message = "您当前代码杀账户为："+ currUser+" 切换账号请按1，注册请按2，自动登录请按3";
+    }
+    var loginStr = prompt(message);
+    var login = parseInt(loginStr);
+    if (login === 2){
         window.open(
             'https://docs.qq.com/doc/DVERHVU5vdkFzVGpX',
             '_blank' // <- This is what makes it open in a new window.
         );
         return;
+    }else if (login === 1){
+        username = prompt("请输入用户");
+        password = prompt("请输入密码");
+    }else{
+        username = currUser;
+        password = currPass;
     }
-    var username = prompt("请输入用户");
-    var password = prompt("请输入密码");
     if (username !== null && password !== null){
         $.getScript("//cdn.jsdelivr.net/npm/leancloud-storage@3.14.0/dist/av-min.js", function(){
             var { Query, User } = AV;
